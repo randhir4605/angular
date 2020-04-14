11f358ad6f65ca8c4a52f287fc021b9b438ed9d6
Randhir <randhir4605@gmail.com>
2020-04-14 16:12:20 +0530

    Angular reactive form demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:ReactiveFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
---
+++ src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.css']
+})
+export class ReactiveFormComponent implements OnInit {
+  singlefield=new FormControl('');
+  
+  profileForm=new FormGroup({
+    firstName:new FormControl(''),
+    lastName:new FormControl('')
+  });
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+  public updateField(){
+    this.singlefield.setValue('Hello');
+  }
+
+  public onSubmit(){
+    console.log(this.profileForm.value);
+  }
+
+}
